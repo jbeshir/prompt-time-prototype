552595fb90a93a4858e61cdd584c936b3be5fd4c
John Beshir <john@beshir.org>
2014-08-30 00:15:53 +0100

    Make minimal, unscalable, active times UI for now.

--- app/src/main/java/org/beshir/prompttime/PromptTime.java
+++ app/src/main/java/org/beshir/prompttime/PromptTime.java
@@ -10,13 +10,9 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 
 public class PromptTime extends ActionBarActivity implements ActionBar.TabListener {
@@ -104,8 +100,14 @@ public class PromptTime extends ActionBarActivity implements ActionBar.TabListen
         @Override
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            // Return a SectionActiveTimesFragment (defined as a static inner class below).
+            switch (position) {
+                case 0:
+                    return SectionActiveTimesFragment.newInstance(position + 1);
+                case 1:
+                    return SectionPromptFragment.newInstance(position + 1);
+            }
+            return null;
         }
 
         @Override
@@ -128,9 +130,9 @@ public class PromptTime extends ActionBarActivity implements ActionBar.TabListen
     }
 
     /**
-     * A placeholder fragment containing a simple view.
+     * A fragment describing the active times section.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class SectionActiveTimesFragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
          * fragment.
@@ -141,23 +143,55 @@ public class PromptTime extends ActionBarActivity implements ActionBar.TabListen
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
+        public static SectionActiveTimesFragment newInstance(int sectionNumber) {
+            SectionActiveTimesFragment fragment = new SectionActiveTimesFragment();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
             fragment.setArguments(args);
             return fragment;
         }
 
-        public PlaceholderFragment() {
+        public SectionActiveTimesFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_prompt_time, container, false);
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_section_active_times, container, false);
             return rootView;
         }
     }
 
+    /**
+     * A fragment describing the active times section.
+     */
+    public static class SectionPromptFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static SectionPromptFragment newInstance(int sectionNumber) {
+            SectionPromptFragment fragment = new SectionPromptFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public SectionPromptFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_section_prompt, container, false);
+            return rootView;
+        }
+    }
 }
